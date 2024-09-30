9e991578d49a7e0bd7997231213df3d032a6d320
svddenu <bomj770@gmail.com>
2024-09-30 23:24:51 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = input('Text your name: ')
+print('Yor name: ' + name.capitalize())
+
+age = 18
+print('Age: '+ str(age))
+
+ageNew = input('Text your age: ')
+print('New age: ' + str(ageNew))
+
+isStudent = True
+print('Is Student: ' + str(isStudent))
