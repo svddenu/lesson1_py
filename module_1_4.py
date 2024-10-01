ad642065bec92e27155b01e3feeba9b7d141e339
svddenu <bomj770@gmail.com>
2024-10-01 13:33:35 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+myString = input('Text here: ')
+print('In this message: ' + str(len(myString)) + ' symbols')
+
+print(myString.upper())
+print(myString.lower())
+print(myString.replace(" ", ""))
+print(myString[0])
+print(myString[-1])
