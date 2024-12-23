ffa697bb0b90e60713105c9422eef0d66c05c0cb
svddenu <bomj770@gmail.com>
2024-12-23 12:56:48 +0300

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,12 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
+    str_number = str(number).lstrip('0')
+    if not str_number:
+        return 1
     first = int(str_number[0])
-    for i in range(len(str_number)):
-        if len(str_number) <= 1:
-            return first
-        else:
-            return first * get_multiplied_digits(int(str_number[1:]))
+    if len(str_number) <= 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
 
 
 print(get_multiplied_digits(40203))
