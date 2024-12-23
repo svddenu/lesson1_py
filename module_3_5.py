eba4d7f6678339e38c8885cc90548d786499cb21
svddenu <bomj770@gmail.com>
2024-12-23 12:59:57 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -6,7 +6,7 @@ def get_multiplied_digits(number):
     if len(str_number) <= 1:
         return first
     else:
-        return first * get_multiplied_digits(int(str_number[1:]))
+        return first * get_multiplied_digits(str_number[1:])
 
 
 print(get_multiplied_digits(40203))
